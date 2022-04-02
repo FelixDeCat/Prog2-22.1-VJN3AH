ca329e18acefc317c727d484170ae486788363f1
escuela@davinci.edu.ar <escuela@davinci.edu.ar>
2022-04-01 21:33:31 -0300

    mis cambios en clase3
    mi desc de ejemplo

--- Assets/Clase3.cs
+++ Assets/Clase3.cs
@@ -7,12 +7,12 @@ public class Clase3 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("juan");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        print("update juan");
     }
 }
